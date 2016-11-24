925b8ff91288d2a12d7c0ae1a73be4e16906ae4b
Eusebiu Schipor <eusebiuschipor@users.noreply.github.com>
2016-11-24 16:46:42 +0200

    Create CatClickerModel.js

---
+++ CatClickerModel.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var CatClickerModel = {
+  currentCat: 0,
+  cats: [
+    {
+      name: 'cat1',
+      picture: 'cat_picture1.jpg',
+      clicks: 0
+    },
+    {
+      name: 'cat2',
+      picture: 'cat_picture2.jpeg',
+      clicks: 0
+    },
+    {
+      name: 'cat3',
+      picture: 'cat_picture3.jpeg',
+      clicks: 0
+    },
+    {
+      name: 'cat4',
+      picture: 'cat_picture4.jpeg',
+      clicks: 0
+    },
+    {
+      name: 'cat5',
+      picture: 'cat_picture5.jpeg',
+      clicks: 0
+    },
+  ]
+};
